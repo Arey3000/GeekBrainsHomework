19e75b985cb63c7a76f303617ca8e0185ca1afc3
Kirill Shakun <shakun.kir@gmail.com>
2021-04-18 21:10:24 +0300

    upd
    upd

--- 3rdhomework/Complex/Program.cs
+++ 3rdhomework/Complex/Program.cs
@@ -11,6 +11,7 @@ namespace Complex
     {
         //а) Дописать структуру Complex, добавив метод вычитания комплексных чисел.Продемонстрировать работу структуры.
         //б) Дописать класс Complex, добавив методы вычитания и произведения чисел.Проверить работу класса.
+        //в) Добавить диалог с использованием switch демонстрирующий работу класса.
         //Выполнил Шакун Кирилл
         static void Main(string[] args)
         {
@@ -50,7 +51,7 @@ namespace Complex
                + cresult.ToString());
 
 
-            Complex res = new Complex();
+            
             Console.WriteLine("Тип операции: сложение(+) , вычитание(-) , умножение(*)");
             var choice = Console.ReadLine();
                 switch (choice)
