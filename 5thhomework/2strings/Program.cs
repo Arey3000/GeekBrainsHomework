9be1f415bd3dd8fbd33b700ecd1bf3134d9d9c40
Kirill Shakun <shakun.kir@gmail.com>
2021-04-26 09:24:52 +0300

    Lesson-5
    выполнены задачи 1,3

---
+++ 5thhomework/2strings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Для двух строк написать метод, определяющий, является ли одна строка перестановкой другой.
+//Выполнил Шакун Кирилл
+namespace _5thhomework
+{
+    class Program : chk
+    {
+        static void Main(string[] args)
+        {
+            chkstring();
+        }
+    }
+}
---
+++ 5thhomework/2strings/chk.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5thhomework
+{
+    internal class chk
+    {
+
+        public static void chkstring()
+        {
+            Console.Write("Введите первую строку:  ");
+            var first = Console.ReadLine();
+            Console.Write("Введите вторую строку:  ");
+            var second = Console.ReadLine();
+            Console.WriteLine("Проверим их");
+            System.Threading.Thread.Sleep(2000);
+            Console.Clear();
+            if (chkstrg(first, second) == true)
+                Console.WriteLine("Строки являются перестановками друг друга.");
+            else
+                Console.WriteLine("Строки состоят из разных символов.");
+            Console.ReadKey();
+        }
+
+        static bool chkstrg(string s1, string s2)
+        { 
+            return s1.Select(Char.ToUpper).OrderBy(x => x).SequenceEqual(s2.Select(Char.ToUpper).OrderBy(x => x));
+        }
+    }
+}
